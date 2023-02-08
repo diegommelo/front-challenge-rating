a4de15431e3f502fb2c82baffbe94225c5381777
Diego Melo <diego.jackchan@gmail.com>
2023-02-08 18:29:43 -0300

    (chore): vite.config

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,6 @@ export default defineConfig({
     }
   },
   base: process.env.NODE_ENV === 'production'
-    ? '/font-challenge-rating' // prod
+    ? '/front-challenge-rating/' // prod
     : '/', // dev
 })

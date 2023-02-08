e50a1ce451e831610c6fbcc28aaf10019d83e6f5
Diego Melo <diego.jackchan@gmail.com>
2023-02-08 18:20:46 -0300

    (chore): vite.config again

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: './front-challenge-rating'
+  baseurl : '/front-challenge-rating'
 })

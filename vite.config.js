2be2204a832531292b9dd191bfeefcdc71bf2636
Diego Melo <diego.jackchan@gmail.com>
2023-02-08 18:19:28 -0300

    (chore): vite.config

--- vite.config.js
+++ vite.config.js
@@ -11,10 +11,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  base: './front-challenge-rating'
 })
-
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/my-project/'
-    : '/'
-}
